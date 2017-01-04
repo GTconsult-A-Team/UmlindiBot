2d68e147c14de8ff8aebae34ffc5a038d3504288
GTconsult-A-Team <Craig@gtconsult.com>
2017-01-04 09:44:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // Add your requirements
+
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
+
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
+//create bot and add dialogs
+/*
+var bot = new builder.BotConnectorBot
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET });
+bot.add('/', new builder.SimpleDialog(function(session){
+    session.send('Hello World');
+}));
+*/
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -8,14 +21,17 @@ server.listen(process.env.PORT || 3000, function()
 {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET }); 
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+server.post('/API/Messages', connector.listen());
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
+bot.dialog('/', new builder.SimpleDialog(function (session) {
     session.send("Hello World");
-});
+}));
